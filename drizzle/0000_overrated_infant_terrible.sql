039fc4cd0fff24d1958b17673310ea65da3725f6
lp <lpcampanholi@gmail.com>
2024-12-23 16:01:35 -0300

    isso mesmo

---
+++ drizzle/0000_overrated_infant_terrible.sql
@@ -0,0 +1,11 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
+CREATE TABLE `users` (
+	`id` int NOT NULL,
+	`name` varchar(250),
+	`email` varchar(250),
+	CONSTRAINT `users_id` PRIMARY KEY(`id`)
+);
+
+*/
